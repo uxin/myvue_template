a5f55a1069b6208da14d04cc39bfa6f109842645
uxin <513928114@qq.com>
2019-07-09 21:08:20 +0800

    添加了全局路由钩子函数,实现了通过指定参数跳转到指定的项目位置

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,13 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
+const routers = new Router({
   routes: [
     {
       path: '/',
       name: 'home',
       component: resolve => require(['@/components/home'], resolve),
-      redirect: "info",  //重定向
+      // redirect: "info",  //重定向
 
       children: [
         {
@@ -28,3 +28,38 @@ export default new Router({
     }
   ]
 })
+
+
+// console.dir(routers)
+
+// beforeEach(()=>{})  //全局路由钩子函数,其中有三个参数
+// 参数1: 到哪个地址去
+// 参数2: 从哪个地址来
+// 参数3: 前往下一个路由
+// 注:此页面用了beforeEach()全局路由钩子函数时,就不要用到重定向了
+// 作用:适用于混合开发,通过参数跳转项目的位置
+
+routers.beforeEach((to, from, next) => {
+  // console.log(to, from);
+  if (to.path == "/") {
+    if (location.search.indexOf("news") != -1) {
+      next({ path: "/news" });
+    } else if (location.search.indexOf("info") != -1) {
+      next({ path: "/info" });
+    } else if (location.search.indexOf("test") != -1) {
+      next({ path: "/test" });
+    } else {
+      next();  // 前往下一个路由
+    }
+  } else {
+    next();  // 前往下一个路由
+  }
+})
+
+// afterEach(()=>{})  跳转之后才执行
+routers.afterEach((to, from) => {
+  console.log("跳转之后执行");
+
+})
+
+export default routers;
