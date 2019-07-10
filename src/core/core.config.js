c2b2f6a9386ee636a1236507ed3dd8054626dbbd
uxin <513928114@qq.com>
2019-07-10 08:48:20 +0800

    创建了一个core文件夹

---
+++ src/core/core.config.js
@@ -0,0 +1,4 @@
+export default (Vue)=>{
+    console.log(Vue);
+
+}
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import core from "@\core\core.config.js"
+vue.use(core);
 
 Vue.config.productionTip = false
 
